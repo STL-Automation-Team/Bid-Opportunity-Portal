59d6fd42b17a2686af24e1958afb6056c6f96e42
Piyush Pushkar <piyush.pushkar@stltech.in>
2025-08-25 11:43:44 +0530

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = 'https://stlsalesnxt.sterliteapps.com:61445';
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'https://stlsalesnxt.sterliteapps.com:61445';
+//export const BASE_URL = 'http://localhost:8080';
